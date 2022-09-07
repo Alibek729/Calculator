d0bc463512c3f654c7cdc9479000146b1a39d5cf
Alibek Kozhambekov <kozambekovalibek@icloud.com>
2022-09-07 13:24:33 +0600

    Initial Commit

---
+++ Task 11.5 Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 11.5 Calculator
+//
+//  Created by Alibek Kozhambekov on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
